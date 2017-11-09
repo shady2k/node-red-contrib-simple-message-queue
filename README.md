13db6d778b2a68dae526b6eaf7417aafd0499eff
shady2k <shady2k@gmail.com>
2017-11-09 13:19:37 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-node-red-contrib-simple-queue
+node-red-contrib-simple-message-queue
 ================================
 
 A simple queue node that store incoming messages in memory queue and
 uses a feedback from a following action to release the next message.
 
-msg: {topic: "frequency", payload: "3.4"}
-
 Inputs
 trigger: If a message is received with this property, one message from queue will be released.
 reset: If a message is received with this property, queue will be cleared.
+ttl: If a message is received with this property, message added to the queue will live in the queue for specific value in milliseconds. The value of the TTL must be a non-negative integer (0 <= n), describing the TTL period in milliseconds. Thus a value of 1000 means that a message added to the queue will live in the queue for 1 second or until it is delivered.
 
 This node is based on https://gist.github.com/dceejay/cea8afa28b7a93ebdc0f - respect to dceejay (Dave Conway-Jones) https://github.com/dceejay
--- simple-message-queue.js
+++ simple-message-queue.js
@@ -15,9 +15,11 @@
 
 module.exports = function(RED) {
 	
-	var EventEmitter = require('events').EventEmitter;
-	
-	function simpleQueueNode(config) {
+	function isNormalInteger(str) {
+	    return /^\+?(0|[1-9]\d*)$/.test(str);
+	}
+
+	function simpleMessageQueueNode(config) {
 
 		RED.nodes.createNode(this, config);
 		this.name = config.name;
@@ -27,6 +29,7 @@ module.exports = function(RED) {
 		node.on("input", function(msg) {
 			var now = Date.now;
 			var context = node.context();
+			var is_sent = false;
 
 			// if queue doesn't exist, create it
 			context.queue = context.queue || [];
@@ -35,32 +38,47 @@ module.exports = function(RED) {
 			if (msg.hasOwnProperty("reset")) {
 			    if (context.queue.length > 0) {
 			        context.queue = [];
-			        return;
 			    }
-			}
-
-			// if the msg is a trigger one release next message
-			if (msg.hasOwnProperty("trigger")) {
-			    if (context.queue.length > 0) {
+			} else if (msg.hasOwnProperty("trigger")) {   // if the msg is a trigger one release next message
+			    while(context.queue.length > 0 && !is_sent) {
 			        var m = context.queue.shift();
-			        if((now() - m._queuetimestamp) < m.ttl) {
+			        var ttl = m.ttl || 0;
+
+			        if((now() - m._queuetimestamp) < ttl || ttl == 0) {
+			            is_sent = true;
 			            node.send(m);
 			        }
 			    }
 			} else {
 		        // Add to queue
+		        var ttl = msg.ttl || 0;
+		        if(!isNormalInteger(ttl)) {
+		        	msg.ttl = 0;
+		        }
+
 		        msg._queuetimestamp = now();
 		        context.queue.push(msg);
+
+		        // Filter overdue messages
+		        var queue_temp = [];
+			    while(context.queue.length > 0) {
+			        var m = context.queue.shift();
+			        var ttl = m.ttl || 0;
+			        
+			        if((now() - m._queuetimestamp) < ttl || ttl == 0) {
+			        	queue_temp.push(m);
+			        }
+			    }
+			    context.queue = queue_temp;
 			}
 
 			node.status({fill:"green",shape:"ring",text: context.queue.length});
 		});
 
 		node.on("close", function() {
-			//close
+			node.status({fill:"green",shape:"ring",text: 0});
 		});
 	}
 
-	RED.nodes.registerType("simpleQueue", simpleQueueNode);
-	
+	RED.nodes.registerType("simple-queue", simpleMessageQueueNode);
 };
