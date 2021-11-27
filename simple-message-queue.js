03074e1cb49dd06fd001b05abe95c7930b9d0985
shady2k <shady2k@gmail.com>
2021-11-27 15:52:41 +0300

    Fixed types in html file

--- simple-message-queue.js
+++ simple-message-queue.js
@@ -13,55 +13,54 @@
   limitations under the License.
 */
 
-module.exports = function(RED) {
-	
+module.exports = function (RED) {
 	function isNormalInteger(str) {
-	    return /^\+?(0|[1-9]\d*)$/.test(str);
+		return /^\+?(0|[1-9]\d*)$/.test(str);
 	}
-	
+
 	function setBusyFalse(smq) {
 		clearTimeout(smq.bypassTimer);
 		smq.bypassTimer = null;
 		smq.isBusy = false;
 	}
-	
+
 	function setBusyTrue(smq) {
 		smq.isBusy = true;
 	}
 
 	function bypassQueue(smq, context, node) {
-		if(smq.bypassInterval > 0 && context.queue.length > 0 && !smq.bypassTimer) {
+		if (smq.bypassInterval > 0 && context.queue.length > 0 && !smq.bypassTimer) {
 			smq.bypassTimer = setTimeout(function bypassSend() {
 				smq.bypassTimer = null;
-				if(context.queue.length > 0) {
+				if (context.queue.length > 0) {
 					var m = context.queue.shift();
 					m["_queueCount"] = context.queue.length;
 					node.send(m);
-					
-					if(context.queue.length == 0) {
+
+					if (context.queue.length == 0) {
 						smq.isBusy = false;
 					}
-					
-					if(context.queue.length > 0) {
+
+					if (context.queue.length > 0) {
 						smq.bypassTimer = setTimeout(bypassSend, smq.bypassInterval);
 					}
 
 					// Update status
-					node.status({fill:"green",shape:"ring",text: context.queue.length});
+					node.status({ fill: "green", shape: "ring", text: context.queue.length });
 				} else {
 					smq.bypassTimer = null;
-					smq.isBusy = false;					
+					smq.isBusy = false;
 				}
 			}, smq.bypassInterval);
 		}
 	}
-	
+
 	function stopBypassTimer(smq) {
 		clearTimeout(smq.bypassTimer);
 		smq.bypassTimer = null;
 	}
 
-	function simpleMessageQueueNode(config) {
+	function SimpleMessageQueueNode(config) {
 
 		RED.nodes.createNode(this, config);
 		var smq = {};
@@ -70,9 +69,9 @@ module.exports = function(RED) {
 		smq.isBusy = false;
 		smq.bypassTimer = null;
 		var node = this;
-		
+
 		// Yes it's true: an incoming message just happened
-		this.on("input", function(msg) {
+		this.on("input", function (msg) {
 			var now = Date.now;
 			var context = node.context();
 
@@ -82,18 +81,18 @@ module.exports = function(RED) {
 
 			// if the msg is a reset, clear queue
 			if (msg.hasOwnProperty("reset")) {
-		        context.queue = [];
-		        setBusyFalse(smq);
-		    } else if (msg.hasOwnProperty("queueCount")) {
-		    	msg["_queueCount"] = context.queue.length;
-		    	node.send(msg);
+				context.queue = [];
+				setBusyFalse(smq);
+			} else if (msg.hasOwnProperty("queueCount")) {
+				msg["_queueCount"] = context.queue.length;
+				node.send(msg);
 			} else if (msg.hasOwnProperty("bypassInterval")) {
 				let re = /^\+?(0|[1-9]\d*)$/;
-				if(re.test(msg.bypassInterval)) {
+				if (re.test(msg.bypassInterval)) {
 					smq.bypassInterval = msg.bypassInterval;
 				}
 			} else if (msg.hasOwnProperty("bypass")) {
-				if(msg.bypass) {
+				if (msg.bypass) {
 					context.is_disabled = true;
 				} else {
 					context.is_disabled = false;
@@ -101,21 +100,21 @@ module.exports = function(RED) {
 					//context.queue = [];
 				}
 			} else if (msg.hasOwnProperty("trigger")) {   // if the msg is a trigger one release next message
-		        // Filter overdue messages
-				context.queue = context.queue.filter(function(x) {
+				// Filter overdue messages
+				context.queue = context.queue.filter(function (x) {
 					return ((now() - x._queuetimestamp) < x.ttl || x.ttl == 0);
 				});
-			    if(context.queue.length > 0) {
-			        var m = context.queue.shift();
-			        m["_queueCount"] = context.queue.length;
-		            node.send(m);
+				if (context.queue.length > 0) {
+					var m = context.queue.shift();
+					m["_queueCount"] = context.queue.length;
+					node.send(m);
 					stopBypassTimer(smq);
 					bypassQueue(smq, context, node);
-			    } else {
-			    	setBusyFalse(smq);
-			    }
+				} else {
+					setBusyFalse(smq);
+				}
 			} else {
-				if(context.is_disabled || (smq.firstMessageBypass && !smq.isBusy)) {
+				if (context.is_disabled || (smq.firstMessageBypass && !smq.isBusy)) {
 					setBusyTrue(smq);
 					msg["_queueCount"] = context.queue.length;
 					node.send(msg);
@@ -124,14 +123,14 @@ module.exports = function(RED) {
 				} else {
 					// Check if ttl value of new message is positive integer
 					var ttl = msg.ttl || 0;
-					if(!isNormalInteger(ttl)) ttl = 0;
+					if (!isNormalInteger(ttl)) ttl = 0;
 
 					msg.ttl = ttl;
 					msg._queuetimestamp = now();
 					context.queue.push(msg); // Add to queue
 
 					// Filter overdue messages
-					context.queue = context.queue.filter( function(x) {
+					context.queue = context.queue.filter(function (x) {
 						return ((now() - x._queuetimestamp) < x.ttl || x.ttl == 0);
 					});
 				}
@@ -139,14 +138,14 @@ module.exports = function(RED) {
 
 			bypassQueue(smq, context, node);
 			// Update status
-			node.status({fill:"green",shape:"ring",text: context.queue.length});
+			node.status({ fill: "green", shape: "ring", text: context.queue.length });
 		});
 
-		this.on("close", function() {
+		this.on("close", function () {
 			// Update status
-			node.status({fill:"green",shape:"ring",text: 0});
+			node.status({ fill: "green", shape: "ring", text: 0 });
 		});
 	}
 
-	RED.nodes.registerType("simple-queue", simpleMessageQueueNode);
+	RED.nodes.registerType("simple-queue", SimpleMessageQueueNode);
 };
